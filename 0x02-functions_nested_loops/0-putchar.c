accc3dd899fad6394f0620b8186befa25531fdd1
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-04-25 08:03:08 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* function declarations */
+int _putchar(char c);
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *word = "_putchar";
+	int i = 0;
+
+	while (word[i])
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
