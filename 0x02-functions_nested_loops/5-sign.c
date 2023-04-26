d269e5b4b2cf62cad322de4565d1a3553b71bd41
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-04-25 17:21:26 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function name
+ * @n: parameters
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
