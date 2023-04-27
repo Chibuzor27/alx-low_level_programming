fc46f55caacc8da52541feb5528dc0c461d5e2db
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-04-27 16:26:47 -0700

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: bool
+ */
+void main(void)
+{
+	int output;
+
+	output = positive_or_negative(0);
+	
+	printf("%d\n", output);
+}
