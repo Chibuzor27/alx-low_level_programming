3f2d70f7aa4e8010d3273e13a6960f2e16ec9aa8
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-05-10 22:49:41 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1) != '\0')
+	if (*(s + 0) == '\0')
 	{
-		_putchar(*(s + 1));
+		return;
 	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*(s + 0));
 }
 
