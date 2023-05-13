126023a7e26dbd26b03c8b91e142d1bbb9a212d5
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-05-12 18:14:10 -0700

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int get_number(char *a);
+
+/**
+ * main - entry point
+ * @argc: arg
+ * @argv: arg
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+
+	if (argc >= 0)
+	{
+		if (argv[1] == NULL || argv[2] == NULL)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		a = get_number(argv[1]);
+		b = get_number(argv[2]);
+
+		printf("%d\n", a * b);
+	}
+	return (0);
+}
+
+/**
+ * get_number - function
+ * @a: arg
+ *
+ * Return: 0
+ */
+int get_number(char *a)
+{
+	int i = 0;
+	int n = 0;
+
+	while (*(a + i) != '\0')
+	{
+		n = (n * 10) + (a[i] - 48);
+		i++;
+	}
+
+	return (n);
+}
