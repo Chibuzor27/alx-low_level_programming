052987ea09228dfdb8aaf3e1ad77d36d98b04bdb
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-05-16 04:12:17 -0700

    update

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * alloc_grid - function
+ * @width: arg1
+ * @height: arg2
+ *
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(height * sizeof(int*));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		int *a = malloc(width * sizeof(int));
+		if (a == NULL)
+		{
+			return (NULL);
+		}
+		arr[i] = a;
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
