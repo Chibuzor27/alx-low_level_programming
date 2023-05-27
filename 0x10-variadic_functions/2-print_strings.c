213ac1923b6ce5206b77bc371040657ef5f88cd7
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-05-27 07:29:46 -0700

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%s",content);
+			printf("%s", content);
 		}
 
 		if (i != n - 1 && separator != NULL)
