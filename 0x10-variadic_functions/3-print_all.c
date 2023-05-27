6ef7f05a837fb3f3a0fe1942dae1736cb71e73d9
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-05-27 10:29:02 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,12 +6,6 @@ void print_int(va_list vl);
 void print_float(va_list vl);
 void print_this_string(va_list vl);
 
-typedef struct p
-{
-	char s;
-	void (*f)(va_list arg);
-} p;
-
 /**
  * printf_all - print
  * @format: arg
@@ -26,18 +20,22 @@ void print_all(const char * const format, ...)
 	{
 		switch (*(format + i))
 		{
-			case 'c': print_char(vl);
-				  break;
-			case 'i': print_int(vl);
-				  break;
-			case 'f': print_float(vl);
-				  break;
-			case 's': print_this_string(vl);
-				  break;
+			case 'c':
+				print_char(vl);
+				break;
+			case 'i':
+				print_int(vl);
+				break;
+			case 'f':
+				print_float(vl);
+				break;
+			case 's':
+				print_this_string(vl);
+				break;
 			default:
-				  i++;
-				  continue;
-				  break;
+				i++;
+				continue;
+				break;
 		}
 
 		if (*(format + i + 1) != '\0')
@@ -84,11 +82,12 @@ void print_float(va_list vl)
 void print_this_string(va_list vl)
 {
 	char *content = va_arg(vl, char*);
+
 	if (content == NULL)
 	{
 		printf("%p", content);
 		return;
 	}
-	
+
 	printf("%s", content);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,21 @@
 #ifndef variadic_h
 #define variadic_h
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_this_string(char *content);
+
+/**
+ * struct p - struct
+ * @s: arg
+ * @f: arg
+ */
+typedef struct p
+{
+	char s;
+	void (*f)(va_list arg);
+} p;
+
 #endif
