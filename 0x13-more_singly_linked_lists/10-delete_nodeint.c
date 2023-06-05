e9d8504e58b9c4a1658105ce7b6072d7e0985bb0
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-06-05 16:58:11 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,6 +9,34 @@
  *
  * Return: 1
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *previous = NULL;
+	listint_t *next = NULL;
+
+	next = *head;
+	while (i < index)
+	{
+		if (next == NULL)
+		{
+			return (-1);
+		}
+		i++;
+		next = next->next;
+		previous = next;
+	}
+
+	if (next == NULL)
+	{
+		return (-1);
+	}
+
+	if (previous != NULL)
+	{
+		previous->next = next->next;
+	}
+	free(next);
+
+	return (1);
 }
