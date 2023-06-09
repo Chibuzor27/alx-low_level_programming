d35eda0f531fe632979a7640201579403f3a2202
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-06-09 15:00:43 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= UINT_MAX)
+	if (*n >= ULONG_MAX || index >= UINT_MAX)
 	{
 		return (-1);
 	}
 
 	*n = ((~(1 << index)) & *n);
-	return (*n);
+	return (1);
 }
