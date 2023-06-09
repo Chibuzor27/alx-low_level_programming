185baea6a16bb15a21656adad72f0fd86d8bc505
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-06-09 14:45:18 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,25 +10,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0;
-	unsigned long int next = *n;
-
 	if (*n > ULONG_MAX || index >= UINT_MAX)
 	{
 		return (-1);
 	}
 
-	while (next > 0)
-	{
-		next >>= 1;
-		i++;
-	}
-
-	if (index > i)
-	{
-		return (-1);
-	}
-
 	*n = ((1 << index) | *n);
-	return (*n);
+	return (1);
 }
