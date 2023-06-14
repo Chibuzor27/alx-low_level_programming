212dd6425ecb053662257f6deace35b8c6c659bd
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-06-14 04:46:33 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,6 +21,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
 	c = malloc(sizeof(char) * letters);
 	if (c == NULL)
 		return (0);
