efbdc738e58d577216941281fc708bba07cc6da6
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-09-23 14:16:41 -0700

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - function
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	long int i;
+	hash_node_t *current;
+	hash_node_t *next;
+
+	if (ht != NULL)
+	{
+		for (i = ht->size - 1; i >= 0; i--)
+		{
+			if (ht->array[i] != NULL)
+			{
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+
+				if (ht->array[i]->next != NULL)
+				{
+					current = ht->array[i]->next;
+					next = current->next;
+					while (next != NULL)
+					{
+						current = next;
+						next = current->next;
+						free(current);
+					}
+					free(ht->array[i]->next);
+				}
+				current = NULL;
+				next = NULL;
+			}
+			free(ht->array[i]);
+		}
+	}
+}
