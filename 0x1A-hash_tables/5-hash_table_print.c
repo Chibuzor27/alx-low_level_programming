e7cacfeacaa409c73e58c632d3eebd119bedb09d
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-09-23 13:34:31 -0700

    update

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int count = 0;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				if (count > 0)
+					printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				count++;
+			}
+		}
+		printf("}\n");
+	}
+}
