15d3cb57126a15b7c72de10c768a0cc1c913dad5
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-11-05 20:49:39 +0100

    update

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: the size
+ * Return: the pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *t;
+
+	if (size == 0)
+		return (NULL);
+
+	t = malloc(sizeof(hash_table_t));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+
+	t->size = size;
+	t->array = calloc(size, sizeof(hash_node_t *));
+
+	if (t->array == NULL)
+	{
+		free(t);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		t->array[i] = NULL;
+
+	return (t);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets a hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = NULL;
+	node->value = NULL;
+	node->next = NULL;
+
+	node->key = malloc(sizeof(char) * (strlen(key) + 1));
+	if (value != NULL)
+		node->value = malloc(sizeof(char) * (strlen(value) + 1));
+	if (node->key == NULL || ((node->value == NULL) && value != NULL))
+	{
+		if (node->key != NULL)
+			free(node->key);
+		if (node->value != NULL)
+			free(node->value);
+		free(node);
+		return (0);
+	}
+	node->key = strcpy(node->key, key);
+	if (value != NULL)
+		node->value = strcpy(node->value, value);
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = node;
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+
+	return (1);
+}
